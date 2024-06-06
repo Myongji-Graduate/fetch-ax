32a6f02f6c76ebb2157b1a10dbf1ff3300858092
gahyuun <pgh123166@naver.com>
2024-06-06 16:44:08 +0900

    feat: implement getContentType

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
+// // Prepare the response
+// const responseHeaders = AxiosHeaders.from(
+//   'getAllResponseHeaders' in request && request.getAllResponseHeaders()
+// );
+// const responseData = !responseType || responseType === 'text' || responseType === 'json' ?
+//   request.responseText : request.response;
+// const response = {
+//   data: responseData,
+//   status: request.status,
+//   statusText: request.statusText,
+//   headers: responseHeaders,
+//   config, // AxiosRequestConfig에 header만 추가된 타입
+//   request // axios 공식 문서를 보면 응답을 생성한 요청이라는데 어디에 사용되는지 모르겠음
+// };
+
+export type NextFetchResponse<T = any> = {
+  data: T;
+  status: number;
+  statusText: string;
+  headers: Headers;
+};
+
+interface NextFetchRequestInit<T = any> extends RequestInit {
+  data?: T;
+  throwError?: boolean;
+  responseJson?: boolean;
+  requestJson?: boolean;
+}
 export type NextFetchDefaultOptions = {
   /**
    * Base URL of fetch. It will be used when the first argument of fetch is relative URL.
@@ -22,35 +50,73 @@ export type NextFetchDefaultOptions = {
    *
    * @public
    */
-  responseJson?: boolean;
-  /**
-   * Request Json of fetch. If the requestJson attribute is true, request data type is json
-   *
-   * @public
-   */
   requestJson?: boolean;
   /**
    * Response Interceptor of fetch. It will be called after response
    *
    * @public
    */
-  responseInterceptor?: (requestArg: RequestInit) => Promise<RequestInit>;
+  responseType?:
+    | 'arraybuffer'
+    | 'blob'
+    | 'json'
+    | 'text'
+    | 'stream'
+    | 'formdata';
+  responseInterceptor?: (response: Response) => Promise<Response>;
   /**
    * Request Interceptor of fetch. It will be called before request
    *
    * @public
    */
-  requestInterceptor?: (responseArg: ResponseInit) => Promise<ResponseInit>;
+  requestInterceptor?: (requestArg: RequestInit) => Promise<RequestInit>;
+};
+
+// createResponse가 할 일
+const getContentType = (responseHeaders: Headers) => {
+  const header = new Headers(responseHeaders);
+  return header.get('Content-Type') as string; // content-type이 없는 경우를 고려 X
+};
+const processResponse = async (
+  fetchResponse: Response,
+  responseType: NextFetchDefaultOptions['responseType'],
+) => {
+  let type = responseType
+    ? responseType
+    : getContentType(fetchResponse.headers);
+
+  let data: ArrayBuffer | JSON;
+
+  // response type을 먼저 확인한다
+  // response type이 없으면 content-type을 확인한다
+  // type을 가지고 data를 가공한 후 반환
 };
 
 export const nextFetch = {
-  create: (defaultOptions: NextFetchDefaultOptions) => {
+  create: (defaultOptions?: NextFetchDefaultOptions) => {
     const instance = {
-      get(): any {
+      async get<T = any>(
+        url: string | URL,
+        ...args: Parameters<typeof fetch>
+      ): Promise<any> {
         // default options를 가지고 options 만들기
-        // request interceptor 실행
-        // 요청
-        // response interceptor 실행
+        let requestArgs: any;
+        if (defaultOptions?.requestInterceptor) {
+          requestArgs = await defaultOptions?.requestInterceptor(requestArgs);
+        }
+
+        // 요청에는 먼저 content type을 확인한다
+        //
+        const fetchResponse = await fetch(url, {
+          method: 'get',
+          body: requestArgs.data,
+        }); // 수정 필요 현재는 response interceptor을 위한 임의 값
+
+        let response = await processResponse(
+          fetchResponse,
+          requestArgs.responseType,
+        );
+
         // 요청 값 반환
       },
       post(): any {},
