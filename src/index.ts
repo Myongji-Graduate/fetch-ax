1ccf9550b34ddf7d80a67a93089741e5bd3727ce
yougyung <youkyeongs@naver.com>
2024-06-07 12:21:35 +0900

    feat: add custom RequestInit

--- src/index.ts
+++ src/index.ts
@@ -52,6 +52,36 @@ export type NextFetchDefaultOptions = {
  */
 export type FetchArgs = [string | URL, RequestInit | undefined];
 
+interface RequestInit {
+  /** next fetch does not have a method attribute because it has http request method. */
+
+  /** A BodyInit object or null to set request's body. */
+  data?: BodyInit | null;
+  /** A string indicating how the request will interact with the browser's cache to set request's cache. */
+  cache?: RequestCache;
+  /** A string indicating whether credentials will be sent with the request always, never, or only when sent to a same-origin URL. Sets request's credentials. */
+  credentials?: RequestCredentials;
+  /** A Headers object, an object literal, or an array of two-item arrays to set request's headers. */
+  headers?: HeadersInit;
+  /** A cryptographic hash of the resource to be fetched by request. Sets request's integrity. */
+  integrity?: string;
+  /** A boolean to set request's keepalive. */
+  keepalive?: boolean;
+  /** A string to indicate whether the request will use CORS, or will be restricted to same-origin URLs. Sets request's mode. */
+  mode?: RequestMode;
+  priority?: RequestPriority;
+  /** A string indicating whether request follows redirects, results in an error upon encountering a redirect, or returns the redirect (in an opaque fashion). Sets request's redirect. */
+  redirect?: RequestRedirect;
+  /** A string whose value is a same-origin URL, "about:client", or the empty string, to set request's referrer. */
+  referrer?: string;
+  /** A referrer policy to set request's referrerPolicy. */
+  referrerPolicy?: ReferrerPolicy;
+  /** An AbortSignal to set request's signal. */
+  signal?: AbortSignal | null;
+  /** Can only be null. Used to disassociate request from any Window. */
+  window?: null;
+}
+
 const applyDefaultOptionsArgs = (
   [url, requestInit]: FetchArgs,
   defaultOptions?: NextFetchDefaultOptions,
@@ -79,13 +109,13 @@ export const nextFetch = {
     const instance = {
       async get(
         url: string | URL,
-        args: Parameters<typeof fetch>,
+        args?: RequestInit,
       ): Promise<Response | any> {
         // 개발 진행 중이므로 any타입을 임의로 추가
 
         // default options를 적용한 args 생성
         const requestArgs = applyDefaultOptionsArgs(
-          [url, args[1]],
+          [url, args],
           defaultOptions,
         );
 
