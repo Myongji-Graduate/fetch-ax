c0ee0801e387d7b30589b8816ad980e3c14ed354
SeonghunYang <didtjdgns852@naver.com>
2024-08-28 13:03:11 +0000

    refactor: update fetchAX get method to use new API endpoint

--- examples/alias_methods.ts
+++ examples/alias_methods.ts
@@ -4,7 +4,7 @@ import fetchAX from '../src';
 
 // 지정된 ID를 가진 유저에 대한 요청
 fetchAX
-  .get('https://jsonplaceholder.typicode.com/todos/1')
+  .get('https://example.com/user?ID=12345')
   .then(function (response) {
     // 성공 핸들링
     console.log('response', response);
@@ -18,7 +18,7 @@ fetchAX
   });
 
 fetchAX
-  .get('https://jsonplaceholder.typicode.com/todos/1', {
+  .get('https://example.com/user', {
     params: {
       ID: 12345,
     },
@@ -33,27 +33,29 @@ fetchAX
     // 항상 실행되는 영역
   });
 
-// async/await 사용을 원한다면, 함수 외부에 `async` 키워드를 추가하세요.
+// async/await
 async function getUser() {
   try {
-    const response = await fetchAX.get('/user?ID=12345');
+    const response = await fetchAX.get('https://example.com/user?ID=12345');
     console.log(response);
   } catch (error) {
     console.error(error);
   }
 }
 
-// fetchAX
-//   .post('/user', {
-//     firstName: 'Fred',
-//     lastName: 'Flintstone',
-//   })
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
+fetchAX
+  .post('/user', {
+    firstName: 'Fred',
+    lastName: 'Flintstone',
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
 
 function getUserAccount() {
   return fetchAX.get('https://jsonplaceholder.typicode.com/todos/1');
